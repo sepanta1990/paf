16c2adf621038c1ee4c18e5310a8f76096baebbd
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 17:44:29 +0100

    added swagger documentation to addPlayer rest endpoint in PlayerController.java

--- src/main/java/com/paf/exercise/exercise/controller/PlayerController.java
+++ src/main/java/com/paf/exercise/exercise/controller/PlayerController.java
@@ -1,9 +1,14 @@
 package com.paf.exercise.exercise.controller;
 
 import com.paf.exercise.exercise.dto.Player;
-import com.paf.exercise.exercise.mapper.PlayerMapper;
 import com.paf.exercise.exercise.service.PlayerService;
+import com.paf.exercise.exercise.util.mapper.PlayerMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Mohammad Fathizadeh 2020-01-04
  */
+@Api(tags = {"Players"})
+@SwaggerDefinition(tags = {
+        @Tag(name = "Players", description = "Operations related to players")
+})
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
@@ -23,6 +32,7 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
+    @ApiOperation(value = "Create a new player", response = Player.class)
     @PostMapping("/")
     public ResponseEntity<Player> addPlayer(@RequestBody Player player) {
         return ResponseEntity.ok(playerMapper.toPlayerDto(playerService.addPlayer(player)));
