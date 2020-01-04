efa263e182c291d773db1d07184210d6375ab0dc
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 18:09:24 +0100

    created TournamentService.java and TournamentServiceTest.java to implement tests and then actual methods related to tournaments

---
+++ src/main/java/com/paf/exercise/exercise/service/TournamentService.java
@@ -0,0 +1,13 @@
+package com.paf.exercise.exercise.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * @author Mohammad Fathizadeh 2020-01-04
+ */
+@Service
+@Transactional
+public class TournamentService {
+}
---
+++ src/test/java/com/paf/exercise/exercise/service/TournamentServiceTest.java
@@ -0,0 +1,13 @@
+package com.paf.exercise.exercise.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Mohammad Fathizadeh 2020-01-04
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TournamentServiceTest {
+}
