0920fc4703e3141e410a51fd9d833ecfb7dd5d1e
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 16:53:51 +0100

    created PlayerController and PlayerControllerTest.java to implement tests and then actual methods related to players

---
+++ src/main/java/com/paf/exercise/exercise/controller/PlayerController.java
@@ -0,0 +1,12 @@
+package com.paf.exercise.exercise.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mohammad Fathizadeh 2020-01-04
+ */
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+}
---
+++ src/test/java/com/paf/exercise/exercise/controller/PlayerControllerTest.java
@@ -0,0 +1,13 @@
+package com.paf.exercise.exercise.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Mohammad Fathizadeh 2020-01-04
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(PlayerController.class)
+public class PlayerControllerTest {
+}
