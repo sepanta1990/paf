747db2f4a2be27ce05f37f3f3a10ec7b32d941b3
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 18:25:40 +0100

    implemented getTournaments method in TournamentService.java

--- src/main/java/com/paf/exercise/exercise/service/TournamentService.java
+++ src/main/java/com/paf/exercise/exercise/service/TournamentService.java
@@ -1,8 +1,13 @@
 package com.paf.exercise.exercise.service;
 
+import com.paf.exercise.exercise.entity.Player;
+import com.paf.exercise.exercise.entity.Tournament;
+import com.paf.exercise.exercise.repository.PlayerRepository;
+import com.paf.exercise.exercise.repository.TournamentRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * @author Mohammad Fathizadeh 2020-01-04
@@ -10,4 +15,17 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class TournamentService {
+
+    private final TournamentRepository tournamentRepository;
+    private final PlayerRepository playerRepository;
+
+    public TournamentService(TournamentRepository tournamentRepository, PlayerRepository playerRepository) {
+        this.tournamentRepository = tournamentRepository;
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Tournament> getTournaments() {
+        return tournamentRepository.findAll();
+    }
+
 }
