15aad03e85f24fbbd61eac8a0fca8543c0282916
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 16:23:11 +0100

    created PlayerService.java and PlayerServiceTest.java

---
+++ src/main/java/com/paf/exercise/exercise/service/PlayerService.java
@@ -0,0 +1,10 @@
+package com.paf.exercise.exercise.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mohammad Fathizadeh 2020-01-04
+ */
+@Service
+public class PlayerService {
+}
