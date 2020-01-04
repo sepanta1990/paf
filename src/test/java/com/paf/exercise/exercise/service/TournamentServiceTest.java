96112eb42f7a54cb9a3af44eea0338d5bec8a0dd
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 18:24:27 +0100

    implement getTournamentsTest method in TournamentServiceTest.java

--- src/test/java/com/paf/exercise/exercise/service/TournamentServiceTest.java
+++ src/test/java/com/paf/exercise/exercise/service/TournamentServiceTest.java
@@ -1,8 +1,20 @@
 package com.paf.exercise.exercise.service;
 
+import com.paf.exercise.exercise.entity.Tournament;
+import com.paf.exercise.exercise.repository.TournamentRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Mohammad Fathizadeh 2020-01-04
@@ -10,4 +22,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class TournamentServiceTest {
+
+    @MockBean
+    private TournamentRepository tournamentRepository;
+
+    @Autowired
+    private TournamentService tournamentService;
+
+    @Test
+    public void getTournamentsTest() {
+        when(tournamentRepository.findAll()).thenReturn(new ArrayList<>());
+        assertTrue(tournamentService.getTournaments().isEmpty());
+
+        Tournament tournament = new Tournament(1, 5, null);
+        when(tournamentRepository.findAll()).thenReturn(Collections.singletonList(tournament));
+        assertArrayEquals(Collections.singletonList(tournament).toArray(), tournamentService.getTournaments().toArray());
+
+    }
 }
