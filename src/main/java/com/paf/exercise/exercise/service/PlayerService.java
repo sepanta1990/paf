7488b7c6578ab3d4f62b012afad093ec31d7dd82
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 16:37:57 +0100

    implementing addPlayer method in PlayerService.java

--- src/main/java/com/paf/exercise/exercise/service/PlayerService.java
+++ src/main/java/com/paf/exercise/exercise/service/PlayerService.java
@@ -1,10 +1,28 @@
 package com.paf.exercise.exercise.service;
 
+import com.paf.exercise.exercise.dto.Player;
+import com.paf.exercise.exercise.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * @author Mohammad Fathizadeh 2020-01-04
  */
 @Service
+@Transactional
 public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public com.paf.exercise.exercise.entity.Player addPlayer(Player player) {
+        com.paf.exercise.exercise.entity.Player playerEnt = new com.paf.exercise.exercise.entity.Player();
+        playerEnt.setName(player.getName());
+        return playerRepository.save(playerEnt);
+    }
+
 }
