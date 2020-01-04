ef49fcd5dfd5767d7dfb7865f31548e19747336a
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-04 17:34:42 +0100

    implemented addPlayer  rest endpoint in PlayerController.java

--- src/main/java/com/paf/exercise/exercise/controller/PlayerController.java
+++ src/main/java/com/paf/exercise/exercise/controller/PlayerController.java
@@ -1,5 +1,12 @@
 package com.paf.exercise.exercise.controller;
 
+import com.paf.exercise.exercise.dto.Player;
+import com.paf.exercise.exercise.mapper.PlayerMapper;
+import com.paf.exercise.exercise.service.PlayerService;
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
+    private final PlayerService playerService;
+    private final PlayerMapper playerMapper = Mappers.getMapper(PlayerMapper.class);
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player) {
+        return ResponseEntity.ok(playerMapper.toPlayerDto(playerService.addPlayer(player)));
+    }
 }
