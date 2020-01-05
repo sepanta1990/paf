4a6b78769c63800855dec8507f0a1a297aafe308
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-05 01:44:42 +0100

    implemented getTournaments rest endpoint in TournamentController.java

--- src/main/java/com/paf/exercise/exercise/controller/TournamentController.java
+++ src/main/java/com/paf/exercise/exercise/controller/TournamentController.java
@@ -1,7 +1,17 @@
 package com.paf.exercise.exercise.controller;
 
+import com.paf.exercise.exercise.dto.Tournament;
+import com.paf.exercise.exercise.service.TournamentService;
+import com.paf.exercise.exercise.util.mapper.TournamentMapper;
+import io.swagger.annotations.ApiOperation;
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Mohammad Fathizadeh 2020-01-04
@@ -9,4 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tournaments")
 public class TournamentController {
+
+    private final TournamentService tournamentService;
+    private final TournamentMapper tournamentMapper = Mappers.getMapper(TournamentMapper.class);
+
+    public TournamentController(TournamentService tournamentService) {
+        this.tournamentService = tournamentService;
+    }
+
+    @ApiOperation(value = "Fetch all tournaments list")
+    @GetMapping
+    public ResponseEntity<List<Tournament>> getTournaments() {
+        return ResponseEntity.status(HttpStatus.OK).body(tournamentMapper.toTournamentDto(tournamentService.getTournaments()));
+    }
+
 }
