ebb360e6d595e677061039d23ce79eed0d29aac1
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-05 17:24:18 +0100

    implemented test methods to test updateTournament REST endpoint in TournamentControllerTest

--- src/test/java/com/paf/exercise/exercise/controller/TournamentControllerTest.java
+++ src/test/java/com/paf/exercise/exercise/controller/TournamentControllerTest.java
@@ -14,6 +14,7 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,6 +26,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -131,6 +133,42 @@ public class TournamentControllerTest {
         this.mockMvc.perform(get("/tournaments/1")).andExpect(status().isNotFound());
         verify(tournamentService, times(1)).getTournamentById(1);
     }
+
+    @Test
+    public void updateTournamentShouldReturnOK() throws Exception {
+        Player player1 = new Player();
+        player1.setName("Mohammad");
+        player1.setId(1);
+
+        Player player2 = new Player();
+        player2.setName("Robert");
+        player2.setId(2);
+
+        Tournament tournament1 = new Tournament();
+        tournament1.setRewardAmount(10);
+        tournament1.setId(1);
+        tournament1.setPlayers(new HashSet<>(Arrays.asList(player1, player2)));
+
+
+        com.paf.exercise.exercise.dto.Tournament requestDto = new com.paf.exercise.exercise.dto.Tournament(null, 10, null);
+        when(tournamentService.updateTournament(1, requestDto)).thenReturn(Optional.of(tournament1));
+        MvcResult requestResult = this.mockMvc.perform(put("/tournaments/1").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(requestDto))).andExpect(status().isOk()).andReturn();
+
+        com.paf.exercise.exercise.dto.Tournament responseDto = parseResponse(requestResult, com.paf.exercise.exercise.dto.Tournament.class);
+        assertEquals(responseDto.getRewardAmount(), requestDto.getRewardAmount());
+        assertEquals(responseDto, tournamentMapper.toTournamentDto(tournament1));
+    }
+
+    @Test
+    public void updateTournamentShouldReturnNotfound() throws Exception {
+        com.paf.exercise.exercise.dto.Tournament requestDto = new com.paf.exercise.exercise.dto.Tournament(null, 10, null);
+        when(tournamentService.updateTournament(1, requestDto)).thenReturn(Optional.empty());
+        this.mockMvc.perform(put("/tournaments/1").contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(requestDto))).andExpect(status().isNotFound());
+
+    }
+
 
     public static <T> T parseResponse(MvcResult result, Class<T> responseClass) {
         try {
