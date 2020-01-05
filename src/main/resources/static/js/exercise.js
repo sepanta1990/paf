2c2375f615a67fc33b3371523d4fb2decd1c24fa
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-01-05 20:24:42 +0100

    some changes in ui so the services can work with new rest endpoints

--- src/main/resources/static/js/exercise.js
+++ src/main/resources/static/js/exercise.js
@@ -1,96 +1,72 @@
 angular.module('exercise', [])
-    .run(function($rootScope, $http) {
+    .run(function ($rootScope, $http) {
 
-        $rootScope.getTournaments = function() {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'getTournaments'
-                }
-            }).then(function(result) {
+        $rootScope.getTournaments = function () {
+            $http.get('/tournaments').then(function (result) {
                 $rootScope.tournaments = result.data;
-                angular.forEach($rootScope.tournaments, function(tournament) {
-                    $rootScope.getPlayersInTournament(tournament.tournament_id);
+                angular.forEach($rootScope.tournaments, function (tournament) {
+                    $rootScope.getPlayersInTournament(tournament.id);
                 });
             });
         };
 
-        $rootScope.addTournament = function(rewardAmount) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'addTournament',
-                    reward_amount: rewardAmount
-                }
-            }).then(function(result) {
+        $rootScope.addTournament = function (rewardAmount) {
+            $http.post('/tournaments/', {
+                rewardAmount: rewardAmount
+            }).then(function (result) {
                 $rootScope.rewardAmount = undefined;
                 $rootScope.getTournaments();
             });
         };
 
-        $rootScope.editTournament = function(tournament) {
-            $rootScope.editTournamentId = tournament.tournament_id;
-            $rootScope.editRewardAmount = tournament.reward_amount;
+        $rootScope.editTournament = function (tournament) {
+            $rootScope.editTournamentId = tournament.id;
+            $rootScope.editRewardAmount = tournament.rewardAmount;
         };
 
-        $rootScope.updateTournament = function(tournamentId, rewardAmount) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'updateTournament',
-                    tournament_id: tournamentId,
-                    reward_amount: rewardAmount
-                }
-            }).then(function(result) {
+        $rootScope.updateTournament = function (tournamentId, rewardAmount) {
+            $http.put('/tournaments/' + tournamentId, {
+                rewardAmount: rewardAmount
+            }).then(function (result) {
                 $rootScope.editTournamentId = undefined;
                 $rootScope.editRewardAmount = undefined;
                 $rootScope.getTournaments();
             });
         };
 
-        $rootScope.removeTournament = function(tournamentId) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'removeTournament',
-                    tournament_id: tournamentId
-                }
-            }).then(function(result) {
+        $rootScope.removeTournament = function (tournamentId) {
+            $http.delete('/tournaments/' + tournamentId).then(function (result) {
                 $rootScope.getTournaments();
             });
         };
 
-        $rootScope.addPlayerIntoTournament = function(tournamentId, playerName) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'addPlayerIntoTournament',
-                    tournament_id: tournamentId,
-                    player_name: playerName
-                }
-            }).then(function(result) {
+        $rootScope.addPlayerIntoTournament = function (tournamentId, playerId) {
+            $http.post('/tournaments/' + tournamentId + '/players/' + playerId, {}).then(function (result) {
                 $rootScope.playerTournamentId = undefined;
+                $rootScope.playerId = undefined;
+                $rootScope.getPlayersInTournament(tournamentId);
+            });
+        };
+
+        $rootScope.removePlayerFromTournament = function (tournamentId, playerId) {
+            $http.delete('/tournaments/' + tournamentId + '/players/' + playerId).then(function (result) {
+                $rootScope.getPlayersInTournament(tournamentId);
+            });
+        };
+
+        $rootScope.addPlayer = function (playerName) {
+            $http.post('/players/', {
+                name: playerName
+            }).then(function (result) {
                 $rootScope.playerName = undefined;
-                $rootScope.getPlayersInTournament(tournamentId);
+                $rootScope.getTournaments();
             });
         };
 
-        $rootScope.removePlayerFromTournament = function(tournamentId, playerId) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'removePlayerFromTournament',
-                    tournament_id: tournamentId,
-                    player_id: playerId
-                }
-            }).then(function(result) {
-                $rootScope.getPlayersInTournament(tournamentId);
-            });
-        };
-
-        $rootScope.getPlayersInTournament = function(tournamentId) {
-            $http.get('/exercise', {
-                params: {
-                    operation: 'getPlayersInTournament',
-                    tournament_id: tournamentId
-                }
-            }).then(function(result) {
-                var index = $rootScope.tournaments.findIndex(function(element) {
-                    return element.tournament_id == tournamentId;
+        $rootScope.getPlayersInTournament = function (tournamentId) {
+            $http.get('/tournaments/' + tournamentId + '/players').then(function (result) {
+                var index = $rootScope.tournaments.findIndex(function (element) {
+                    return element.id == tournamentId;
                 });
                 $rootScope.tournaments[index].players = result.data;
             });
